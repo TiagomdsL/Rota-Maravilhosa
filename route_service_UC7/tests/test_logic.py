e0fb8a581cf011dcb21b55b35f9416b8c471a262
Rodrigo Freitas <rfafreitas2004@gmail.com>
2026-05-26 15:09:23 +0000

    Tests on UC7 service CD 11.0

--- route_service_UC7/tests/test_logic.py
+++ route_service_UC7/tests/test_logic.py
@@ -114,3 +114,6 @@ def test_interpolate_waypoints_horizontal():
     assert waypoints[0]["latitude"] == approx(0.0)
     assert waypoints[-1]["latitude"] == approx(0.0)
     assert waypoints[-1]["longitude"] == approx(10.0)
+
+
+# test
